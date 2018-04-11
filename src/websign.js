0a28cead52bc7aa1b8b816ead979d7c82720b200
Lucas Perez <lucascperez@gmail.com>
2018-04-11 13:47:04 -0300

    changing to event way

--- src/websign.js
+++ src/websign.js
@@ -1,22 +1,64 @@
 import sha256 from 'js-sha256';
-import { WebSocket, Server, SocketIO } from 'mock-socket';
+import io from 'socket.io-client'
+import EventEmitter from 'event-emitter-es6'
 
-class WebSign {
-  sign(data) {
-    return new Promise((resolve, reject) => {
-        //conecto no socket io
-        const socket = new SocketIO('http://localhost:8080');
-        //registro o ouvinte
-        socket.on('signed-data', signedData => {
-          console.log("aqui dentro", signedData)
-          resolve(signedData)
-        });
 
-        //aqui devo fazer a chamada ao server real
-        let hashedData = sha256(data)
+class WebSign extends EventEmitter {
+  constructor() {
+    super()
 
+    var socket = io('http://127.0.0.1:7721')
+    let that = this
+    socket.on('connect', function(err) {
+      that.emit('service-available', err);
     });
+
+    socket.io.on('connect_error', function(err) {
+      that.emit('service-unavailable', err);
+    })
+    //   this.emit('teste', {data: 'retorno'});
+    //   console.log("testeeeeee do connect")
+    // });
+
+    // var socket = io('http://127.0.0.1:7721')
+
+    // socket.io.on('connect_error', function(err) {
+    //   console.log("DEU RUIM NA CONEXAO COM O SERVER");
+    // })
+    // socket.on('connect', function(err) {
+    //   console.log("CONECTEI NO SERVER");
+    // })
+    // socket.on('signed data', data => {
+    //   console.log("ASSINEI A MENSAGEM");
+    // })
+
+    // this.promises = {}
+    // this.socket = io('http://localhost:7721');
+    // this.socket.on('signed-data', signedData => {
+    //   console.log("lalalala")
+    //   // console.log("aqui dentro", signedData)
+    //   // console.log(this.promises[signedData.hash])
+    //   // console.log(Object.getPrototypeOf(this.promises[signedData.hash]))
+    //   // this.promises[signedData.hash].thenResolve(signedData)
+    //   // this.promises[signedData.hash].resolve(signedData)
+    // });
+  }
+
+  sign(data) {
+    // console.log(data)
+    //aqui devo fazer a chamada ao server real
+    // let hashedData = sha256(data)
+    // let deferred = Q.defer()
+    // this.promises[hashedData] = deferred.promise
+    // // this.mockServer()
+    // this.socket.emit("signed-data", {msg: data, hash:hashedData})
+    // return deferred.promise
+  }
+
+  mockServer(data, hashedData) {
+    console.log(this.socket.io)
+    this.socket.emit("signed-data", {msg: data, hash:hashedData})
   }
 }
 
-export default new WebSign()
+export default WebSign
--- test/websign.spec.js
+++ test/websign.spec.js
@@ -1,36 +1,145 @@
-import { SocketIO, Server } from 'mock-socket';
+// import { SocketIO, Server } from 'mock-socket';
 import WebSign from '../src/websign';
 import sha256 from 'js-sha256';
 import {expect} from 'chai';
+import socketio from 'socket.io';
+import http from 'http';
 
-describe('#sign', () => {
+describe('websign tests', () => {
   
-  var mockServer
+  const server = http.createServer();
 
-  beforeEach(() => {
-    window.io = SocketIO;
-    mockServer = new Server('http://localhost:8080');
-  })
-  
-  afterEach(done => {
-    mockServer.stop(done);
-  })
-
-  describe("when user wants to sign one message", () => {
-    it('should get the signed message in a promise', (done) => {
-      const message1 = "test message 01"
-      const hashedMessage1 = sha256(message1)
-
-      WebSign.sign(message1)
-        .then(data => {
-          expect(data.msg).to.equal(message1)
-        }).catch(error => {
-          console.log(error)
-        });
-
-      mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1});
-
-      done()
+  function startServer() {
+    server.listen(7721, 'localhost');
+    const io = socketio(server);
+    io.origins('*:*');
+    io.on('connection', socket => {
+      socket.on('sign', data => {
+        console.log(socket, data)
+      });
     });
-  });
+  }
+
+  function closeServer(done) {
+    //encerrando o server
+    done()
+    server.close(function () {
+      console.log('server stopped');
+    });
+  }
+
+  describe("when user instantiate the lib", () => {
+    describe("and server is ready", () => {
+      
+      it('should be able to listen service-available event', (done) => {
+        startServer();
+        const webSign = new WebSign()
+        webSign.on('service-available', err => {
+          // expect(err).to.be undefined
+          expect(err).to.equal(undefined)
+
+          closeServer(done);
+        }); 
+      })
+    })
+
+    describe("and server is not ready", () => {
+      it('should be able to listen service-unavailable event', (done) => {
+        const webSign = new WebSign()
+        webSign.on('service-unavailable', err => {
+          expect(err.description).to.equal(503)
+          closeServer(done)
+        });  
+      })
+    })
+  })
+
+
+  // xdescribe("when user wants to sign one message", () => {
+  //   it('should get the signed message in a promise', (done) => {
+  //     const message1 = "test message 01"
+  //     const hashedMessage1 = sha256(message1)
+
+  //     new WebSign().sign(message1).then(response => {
+  //       console.log("resposta da promise", response)
+  //     })
+
+  //     mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1});
+
+  //     done()
+  //   });
+  // });
+
+  // describe("when user wants to sign 2 messages", () => {
+  //   it('should get the signed message in a promise', (done) => {
+
+  //     server.listen(7721, 'localhost');
+  //     const io = socketio(server);
+  //     io.origins('*:*');
+  //     io.on('connection', socket => {
+  //       socket.on('sign', data => {
+  //         // cb(socket.request.headers.referer, data, socket);
+  //         console.log(socket, data)
+  //       });
+  //     });
+
+  //     const webSign = new WebSign()
+
+  //     webSign.on('connect', data => {
+
+  //       console.log(data)
+
+  //       //encerrando o server
+  //       done()
+  //       server.close(function () {
+  //         console.log('server stopped');
+  //       });
+
+  //     });
+
+  //     //disparo o evento
+  //     // webSign.fireTeste()
+
+  //     // const message1 = "test message 01"
+  //     // const hashedMessage1 = sha256(message1)
+
+  //     // const message2 = "test message 02"
+  //     // const hashedMessage2 = sha256(message2)
+
+  //     // const webSign = new WebSign()
+
+  //     // // setTimeout(() => {
+  //     // //   mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1})
+  //     // // }, 1000);
+
+  //     // // mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1})
+
+  //     // // mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1})
+      
+  //     // webSign.sign(message1).then(response => {
+  //     //   console.log("resposta da promise 1", response)
+        
+  //     // })
+
+  //     // webSign.sign(message2).then(response => {
+  //     //   console.log("resposta da promise 2", response)
+  //     //   done()
+  //     // })
+
+
+
+  //     // setTimeout(() => {
+  //     //   mockServer.emit("signed-data", {msg: message2, hash: hashedMessage2})
+  //     // }, 1500);
+
+
+      
+      
+
+      
+      
+
+      
+  //   });
+  // });
 });
