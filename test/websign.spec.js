886e11701cdfeb905e24af611ea7e96ace651725
Lucas Perez <lucascperez@gmail.com>
2018-04-11 13:48:17 -0300

    removimeng comments++

--- test/websign.spec.js
+++ test/websign.spec.js
@@ -34,9 +34,7 @@ describe('websign tests', () => {
         startServer();
         const webSign = new WebSign()
         webSign.on('service-available', err => {
-          // expect(err).to.be undefined
           expect(err).to.equal(undefined)
-
           closeServer(done);
         }); 
       })
@@ -52,93 +50,4 @@ describe('websign tests', () => {
       })
     })
   })
-
-
-  // xdescribe("when user wants to sign one message", () => {
-  //   it('should get the signed message in a promise', (done) => {
-  //     const message1 = "test message 01"
-  //     const hashedMessage1 = sha256(message1)
-
-  //     new WebSign().sign(message1).then(response => {
-  //       console.log("resposta da promise", response)
-  //     })
-
-  //     mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1});
-
-  //     done()
-  //   });
-  // });
-
-  // describe("when user wants to sign 2 messages", () => {
-  //   it('should get the signed message in a promise', (done) => {
-
-  //     server.listen(7721, 'localhost');
-  //     const io = socketio(server);
-  //     io.origins('*:*');
-  //     io.on('connection', socket => {
-  //       socket.on('sign', data => {
-  //         // cb(socket.request.headers.referer, data, socket);
-  //         console.log(socket, data)
-  //       });
-  //     });
-
-  //     const webSign = new WebSign()
-
-  //     webSign.on('connect', data => {
-
-  //       console.log(data)
-
-  //       //encerrando o server
-  //       done()
-  //       server.close(function () {
-  //         console.log('server stopped');
-  //       });
-
-  //     });
-
-  //     //disparo o evento
-  //     // webSign.fireTeste()
-
-  //     // const message1 = "test message 01"
-  //     // const hashedMessage1 = sha256(message1)
-
-  //     // const message2 = "test message 02"
-  //     // const hashedMessage2 = sha256(message2)
-
-  //     // const webSign = new WebSign()
-
-  //     // // setTimeout(() => {
-  //     // //   mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1})
-  //     // // }, 1000);
-
-  //     // // mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1})
-
-  //     // // mockServer.emit("signed-data", {msg: message1, hash: hashedMessage1})
-      
-  //     // webSign.sign(message1).then(response => {
-  //     //   console.log("resposta da promise 1", response)
-        
-  //     // })
-
-  //     // webSign.sign(message2).then(response => {
-  //     //   console.log("resposta da promise 2", response)
-  //     //   done()
-  //     // })
-
-
-
-  //     // setTimeout(() => {
-  //     //   mockServer.emit("signed-data", {msg: message2, hash: hashedMessage2})
-  //     // }, 1500);
-
-
-      
-      
-
-      
-      
-
-      
-  //   });
-  // });
 });
