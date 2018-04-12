b8cd84257ac76704134716e5efa2e47b6280b1dd
Lucas Perez <lucascperez@gmail.com>
2018-04-12 09:09:04 -0300

    ajustes na lib

--- src/websign.js
+++ src/websign.js
@@ -7,57 +7,25 @@ class WebSign extends EventEmitter {
   constructor() {
     super()
 
-    var socket = io('http://127.0.0.1:7721')
+    this.socket = io('http://127.0.0.1:7721')
+    
     let that = this
-    socket.on('connect', function(err) {
+
+    this.socket.on('connect', function(err) {
       that.emit('service-available', err);
     });
 
-    socket.io.on('connect_error', function(err) {
+    this.socket.io.on('connect_error', function(err) {
       that.emit('service-unavailable', err);
     })
-    //   this.emit('teste', {data: 'retorno'});
-    //   console.log("testeeeeee do connect")
-    // });
 
-    // var socket = io('http://127.0.0.1:7721')
-
-    // socket.io.on('connect_error', function(err) {
-    //   console.log("DEU RUIM NA CONEXAO COM O SERVER");
-    // })
-    // socket.on('connect', function(err) {
-    //   console.log("CONECTEI NO SERVER");
-    // })
-    // socket.on('signed data', data => {
-    //   console.log("ASSINEI A MENSAGEM");
-    // })
-
-    // this.promises = {}
-    // this.socket = io('http://localhost:7721');
-    // this.socket.on('signed-data', signedData => {
-    //   console.log("lalalala")
-    //   // console.log("aqui dentro", signedData)
-    //   // console.log(this.promises[signedData.hash])
-    //   // console.log(Object.getPrototypeOf(this.promises[signedData.hash]))
-    //   // this.promises[signedData.hash].thenResolve(signedData)
-    //   // this.promises[signedData.hash].resolve(signedData)
-    // });
+    this.socket.on('signed data', data => {
+      that.emit('signed-data', data);
+    })
   }
 
   sign(data) {
-    // console.log(data)
-    //aqui devo fazer a chamada ao server real
-    // let hashedData = sha256(data)
-    // let deferred = Q.defer()
-    // this.promises[hashedData] = deferred.promise
-    // // this.mockServer()
-    // this.socket.emit("signed-data", {msg: data, hash:hashedData})
-    // return deferred.promise
-  }
-
-  mockServer(data, hashedData) {
-    console.log(this.socket.io)
-    this.socket.emit("signed-data", {msg: data, hash:hashedData})
+    this.socket.emit('sign', data)
   }
 }
 
--- test/websign.spec.js
+++ test/websign.spec.js
@@ -8,45 +8,69 @@ import http from 'http';
 describe('websign tests', () => {
   
   const server = http.createServer();
+  let globalSocket = null;
+  let webSign = null;
 
   function startServer() {
     server.listen(7721, 'localhost');
     const io = socketio(server);
     io.origins('*:*');
     io.on('connection', socket => {
+      globalSocket = socket
       socket.on('sign', data => {
-        console.log(socket, data)
+        const signedData = sha256(data)
+        socket.emit('signed data', signedData)
       });
     });
+
+    webSign = new WebSign()
   }
 
   function closeServer(done) {
     done()
-    server.close(function () {
+    server.close(() => {
       console.log('server stopped');
     });
+    webSign.destroy();
   }
 
   describe("when user instantiate the lib", () => {
     describe("and server is ready", () => {
-      
       it('should be able to listen service-available event', (done) => {
         startServer();
-        const webSign = new WebSign()
+        
         webSign.on('service-available', err => {
           expect(err).to.equal(undefined)
           closeServer(done);
         }); 
       })
+
+
+      it('should be able to listen signed-data event', (done) => {
+        
+        const data = "test-message"
+        const expectedSignedData = sha256(data)
+
+        startServer();
+
+        webSign.on('signed-data', response => {
+          expect(response).to.equal(expectedSignedData)
+          closeServer(done);
+        }); 
+
+        webSign.sign(data)
+      })
     })
 
     describe("and server is not ready", () => {
       it('should be able to listen service-unavailable event', (done) => {
-        const webSign = new WebSign()
+        startServer();
         webSign.on('service-unavailable', err => {
           expect(err.description).to.equal(503)
           closeServer(done)
-        });  
+        });
+
+        globalSocket.emit('connect_error')
       })
     })
   })
